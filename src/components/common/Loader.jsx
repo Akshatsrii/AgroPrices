c5f1b820b1564edcd374a27f54f12ec2d82de86c
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-04-12 19:37:36 +0530

    updated the loader page

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,10 +1,16 @@
 function Loader() {
-  return <div className="text-center p-4">Loading...</div>
+  return (
+    <div className="flex items-center justify-center p-4">
+      <div className="h-6 w-6 border-2 border-green-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  )
 }
 
-export function Skeleton() {
+export function Skeleton({ className = "" }) {
   return (
-    <div className="animate-pulse bg-white/10 h-20 w-full rounded-xl"></div>
+    <div
+      className={`animate-pulse bg-white/10 rounded-xl ${className}`}
+    ></div>
   )
 }
 
