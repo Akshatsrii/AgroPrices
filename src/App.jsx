eadc57dee091bb3b8e8ec2cde433520af88db0ca
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-04-12 20:20:58 +0530

    app.jsx routing is changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer";
+import React, { useEffect } from "react"
+import { useLocation } from "react-router-dom"
+import AppRoutes from "./routes/AppRoutes"
+import Navbar from "./components/common/Navbar"
+import Footer from "./components/common/Footer"
 
+// 🔹 Scroll to top on route change
 function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname } = useLocation()
+
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [pathname]);
-  return null;
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }, [pathname])
+
+  return null
 }
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-cream">
+    <div className="flex flex-col min-h-screen bg-cream text-gray-800">
       <ScrollToTop />
+
+      {/* 🔹 Navbar */}
       <Navbar />
+
+      {/* 🔹 Main Content */}
       <main className="flex-1">
         <AppRoutes />
       </main>
+
+      {/* 🔹 Footer */}
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
